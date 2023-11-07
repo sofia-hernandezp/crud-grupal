09a4e960215d987d0b3198530dc84cc3190141d2
felipechak <felipepacin7@gmail.com>
2023-11-06 21:12:41 -0300

    Termino de implementar

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded",()=>{
     const inputPutNombre = document.getElementById('inputPutNombre')
     const inputPutApellido = document.getElementById('inputPutApellido')
     const btnSendChanges = document.getElementById('btnSendChanges')
+    const modalPut = new bootstrap.Modal(document.getElementById('dataModal'), {
+        keyboard: false
+      })
 
     inputPutId.addEventListener("input",()=>{
         btnPut.disabled = inputPutId.value == ""
@@ -15,8 +18,26 @@ document.addEventListener("DOMContentLoaded",()=>{
         btnSendChanges.disabled = inputPutApellido.value == ""
     })
     btnPut.addEventListener("click",()=>{
-       inputPutNombre.value = "Juan"
-       inputPutApellido.value = "Carlos"
-       btnSendChanges.disabled = inputPutNombre.value == "" && inputPutApellido.value == ""
+        fetch('https://65497572e182221f8d519410.mockapi.io/users/'+inputPutId.value).then((res)=>{res.json().then((record)=>{
+            inputPutNombre.value = record.name
+            inputPutApellido.value = record.lastname
+            btnSendChanges.disabled = inputPutNombre.value == "" && inputPutApellido.value == ""
+            modalPut.show()
+        })
+        })
+    })
+    btnSendChanges.addEventListener("click",()=>{
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name: inputPutNombre.value, lastname: inputPutApellido.value})
+        };
+        fetch('https://65497572e182221f8d519410.mockapi.io/users/'+inputPutId.value, requestOptions)
+            .then(response => {
+                if (response.status != 200) {
+                    console.log("ERROR")
+                }
+                modalPut.hide()
+            })
     })
 })
